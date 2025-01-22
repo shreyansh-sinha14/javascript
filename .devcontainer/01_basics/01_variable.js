c77110e4323c8b60e653be36c274ab6faef3ceae
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2025-01-22 17:46:06 +0000

    container config

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=1142678
+let accountEmail="sinha.14shreyansh@gamil.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+//  path -> /workspaces/javascript/.devcontainer/01_basics/01_variable.js
+//  by declaring const it cannot be changed later
+/* prefer not to use var 
+because of issue in block scope and functional scope
+*/
