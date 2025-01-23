d99f81e433aebe24b54c9babd0b8a1e9505e55fa
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2025-01-23 16:49:00 +0000

    container config

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33"
+console.log(typeof score);
+
+// coversion of string to number
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// for conversion of "33"=>33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true; 0=> false
+// "" => false
+// "ss" => true
+
+
+let numberS = 33
+let stringNumber = String(numberS)
+console.log(stringNumber);
+console.log(typeof stringNumber);
