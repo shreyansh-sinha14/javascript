d0045546fffbab82ba03e35b884bb833bd68fcf6
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2025-01-23 07:07:06 +0000

    container config

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"user strict"; //treat all JS code as newer version
+
+//console.log(3+3) we are using nodejs not browser
+
+let namee="Sam"
+let age=19
+let isLoggedIn=false
+
+console.table([namee, age, isLoggedIn])
+
+//number => 2 to power 53
+// bigint
+//string=""
+//boolean=true/false
+//null =standalone value   ITS TYPE IS DEFINED AS OBJECT
+//undefined=               ITS TYPE IS DEFINED AS UNDEFINED
+//symbol=>unique
+//object
