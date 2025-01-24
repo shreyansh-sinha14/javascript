345f0d55865d0379733e34fb86bdd8062777f4e9
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2025-01-24 06:29:13 +0000

    container config

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,32 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log(("02">1));
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+// equality check == and comarisons > < >= <= work differently
+// comparisons convert null to a number, treating it as 0. That's why null>=0 is true and null>0 is false
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// ===(strict check)
+// it checks the value the as well as the datatype
+console.log("2"===2);
+
+
+
+
+
+
+
+
+
+
